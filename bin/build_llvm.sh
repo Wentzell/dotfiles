32e0ff8036c3b897c5bb00bf10c9d6d314c36a76
Nils Wentzell <nwentzell@flatironinstitute.org>
2019-05-17 17:06:41 -0400

    Additional cleaning of build_llvm.sh

--- bin/build_llvm.sh
+++ bin/build_llvm.sh
@@ -1,56 +1,57 @@
 WORK_DIR=$PWD
-mkdir -p ${WORK_DIR}
+RELEASE=trunk #branches/release_80 #trunk
 
 cd ${WORK_DIR}
-svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm
+svn co http://llvm.org/svn/llvm-project/llvm/${RELEASE} llvm
 
 cd ${WORK_DIR}
 cd llvm/tools
-svn co http://llvm.org/svn/llvm-project/cfe/trunk clang
+svn co http://llvm.org/svn/llvm-project/cfe/${RELEASE} clang
 
 cd ${WORK_DIR}
 cd llvm/tools/clang/tools
-svn co http://llvm.org/svn/llvm-project/clang-tools-extra/trunk extra
+svn co http://llvm.org/svn/llvm-project/clang-tools-extra/${RELEASE} extra
 
-cd ${WORK_DIR}
-cd llvm/tools/clang/tools
-mkdir templight
-git clone https://github.com/mikael-s-persson/templight templight
-echo "add_clang_subdirectory(templight)" >> CMakeLists.txt
+#cd ${WORK_DIR}
+#cd llvm/tools/clang/tools
+#mkdir templight
+#git clone https://github.com/mikael-s-persson/templight templight
+#echo "add_clang_subdirectory(templight)" >> CMakeLists.txt
 
-cd ${WORK_DIR}
-cd llvm/tools
-git clone https://github.com/facebookincubator/BOLT llvm-bolt
-cd ..
-patch -p 1 < tools/llvm-bolt/llvm.patch
-
-cd ${WORK_DIR}
-cd llvm/tools
-svn co http://llvm.org/svn/llvm-project/lld/trunk lld
+#cd ${WORK_DIR}
+#cd llvm/tools
+#git clone https://github.com/facebookincubator/BOLT llvm-bolt
+#cd ..
+#patch -p 1 < tools/llvm-bolt/llvm.patch
 
 cd ${WORK_DIR}
 cd llvm/tools
-svn co http://llvm.org/svn/llvm-project/polly/trunk polly
+svn co http://llvm.org/svn/llvm-project/lld/${RELEASE} lld
+
+cd ${WORK_DIR}
+cd llvm/tools
+svn co http://llvm.org/svn/llvm-project/polly/${RELEASE} polly
 
 cd ${WORK_DIR}
 cd llvm/projects
-svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt
+svn co http://llvm.org/svn/llvm-project/compiler-rt/${RELEASE} compiler-rt
 
 cd ${WORK_DIR}
 cd llvm/projects
-svn co http://llvm.org/svn/llvm-project/openmp/trunk openmp
+svn co http://llvm.org/svn/llvm-project/openmp/${RELEASE} openmp
 
 cd ${WORK_DIR}
 cd llvm/projects
-svn co http://llvm.org/svn/llvm-project/libcxx/trunk libcxx
-svn co http://llvm.org/svn/llvm-project/libcxxabi/trunk libcxxabi
+svn co http://llvm.org/svn/llvm-project/libcxx/${RELEASE} libcxx
+svn co http://llvm.org/svn/llvm-project/libcxxabi/${RELEASE} libcxxabi
 
-BUILD_DIR=$PWD/llvm_build
-INSTALL_DIR=$HOME/opt/llvm
+BUILD_DIR=${WORK_DIR}/llvm_build
+INSTALL_DIR=$HOME/opt/llvm_${RELEASE}
 TRHEADS=40
 
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
+# Cf. https://llvm.org/docs/CMake.html
 cmake -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_C_FLAGS="-O3 -march=native" \
       -DCMAKE_CXX_FLAGS="-O3 -march=native" \
@@ -69,10 +70,10 @@ cmake -DCMAKE_BUILD_TYPE=Release \
       -DLLVM_ENABLE_BINDINGS=OFF \
       -DLLVM_OPTIMIZED_TABLEGEN=ON \
       -DLLVM_USE_LINKER=gold \
+      -DCMAKE_C_COMPILER="${CC}" \
+      -DCMAKE_CXX_COMPILER="${CXX}" \
       "${WORK_DIR}/llvm"
       #-DLINK_POLLY_INTO_TOOLS=ON \
-      #-DCMAKE_C_COMPILER="${CC}" \
-      #-DCMAKE_CXX_COMPILER="${CXX}" \
       #-DLLVM_BINUTILS_INCDIR="${WORK_DIR}/llvm/tools/binutils/include" \
       #-DLLVM_TARGETS_TO_BUILD="ARM;AArch64;PowerPC;X86" \
       #-DLLVM_VERSION_SUFFIX="-r${LLVM_SVN_REVISION:?}" \
@@ -80,4 +81,4 @@ cmake -DCMAKE_BUILD_TYPE=Release \
       #-DCLANG_VENDOR="${VENDOR:+"${VENDOR} "}" \
       #-DCLANG_VERSION_SUFFIX="-r${CLANG_SVN_REVISION:?}" \
 
-make -j ${THREADS} install
+make install
