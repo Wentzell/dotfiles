2b7fcbec0fd093cafb703ea79bbcbf350ba02c6f
Nils Wentzell <nwentzell@flatironinstitute.org>
2019-09-17 15:24:50 -0400

    Some cleaning in build_llvm.sh, add march=native and optional cuda arch flags

--- bin/build_llvm.sh
+++ bin/build_llvm.sh
@@ -24,7 +24,8 @@ git clone https://github.com/llvm/llvm-project --branch $RELEASE --depth 1
 # Build with gcc
 export CC=gcc
 export CXX=g++
-export CXXFLAGS=''
+export CFLAGS='-O3 -march=native'
+export CXXFLAGS='-O3 -march=native'
 GCC_INSTALL_PREFIX=$(dirname $(which gcc))/../
 
 mkdir -p ${BUILD_DIR}
@@ -32,15 +33,13 @@ cd ${BUILD_DIR}
 # Cf. https://llvm.org/docs/CMake.html
 # and https://llvm.org/docs/GettingStarted.html
 cmake -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" \
       -DGCC_INSTALL_PREFIX=${GCC_INSTALL_PREFIX} \
       -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;lld;polly;compiler-rt;openmp;libcxx;libcxxabi" \
-      -DCMAKE_C_FLAGS="-O3" \
-      -DCMAKE_CXX_FLAGS="-O3" \
-      -DLLVM_CCACHE_BUILD=ON \
-      -DLLVM_ENABLE_PIC=ON \
-      -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" \
       -DLLVM_PARALLEL_COMPILE_JOBS="${THREADS}" \
       -DLLVM_PARALLEL_LINK_JOBS="${THREADS}" \
+      -DLLVM_CCACHE_BUILD=ON \
+      -DLLVM_ENABLE_PIC=ON \
       -DLLVM_ENABLE_THREADS=ON \
       -DLLVM_ENABLE_WARNINGS=OFF \
       -DLLVM_ENABLE_WERROR=OFF \
@@ -55,8 +54,6 @@ cmake -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_CXX_COMPILER="${CXX}" \
       -DLIBOMP_TSAN_SUPPORT=1 \
       "${SRC_DIR}/llvm-project/llvm"
-      #-DCLANG_OPENMP_NVPTX_DEFAULT_ARCH=sm_61 \
-      #-DLIBOMPTARGET_NVPTX_COMPUTE_CAPABILITIES=61 \
       #-DLINK_POLLY_INTO_TOOLS=ON \
       #-DLLVM_BINUTILS_INCDIR="${SRC_DIR}/llvm/tools/binutils/include" \
       #-DLLVM_TARGETS_TO_BUILD="ARM;AArch64;PowerPC;X86" \
