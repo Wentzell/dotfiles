2e6dd92518d57b997f7641b16e6680acc7daf48a
Nils Wentzell <nwentzell@flatironinstitute.org>
2022-07-07 17:03:45 -0400

    Add build script for neovim

---
+++ bin/build_nvim.sh
@@ -0,0 +1,14 @@
+VERSION=master
+PREFIX=~/opt/neovim
+
+git clone https://github.com/neovim/neovim --depth 1 --branch $VERSION
+cd neovim
+
+mkdir .deps && cd .deps
+cmake ../cmake.deps
+make
+cd ..
+
+mkdir build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX
+make -j 20 install
